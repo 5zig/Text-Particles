6d1a9716927c9b7bf4730f53a645a8b7fc9f11fe
5zig <alex.albers@me.com>
2014-09-12 17:31:13 +0200

    Initial Commit

---
+++ src/eu/mc5zig/particletext/Main.java
@@ -0,0 +1,17 @@
+package eu.mc5zig.particletext;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
