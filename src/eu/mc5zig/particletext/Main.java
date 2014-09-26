e7d58bd76842f768b43585123d3a1b84a0096410
5zig <alex.albers@me.com>
2014-09-26 18:52:53 +0200

    Removed '/test' command

--- src/eu/mc5zig/particletext/Main.java
+++ src/eu/mc5zig/particletext/Main.java
@@ -2,9 +2,6 @@ package eu.mc5zig.particletext;
 
 import java.util.logging.Level;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import eu.mc5zig.particletext.characters.CharacterManager;
@@ -26,23 +23,6 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onDisable() {
 
-	}
-
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("test")) {
-			if (sender instanceof Player) {
-				Player player = (Player) sender;
-				if (args.length > 0) {
-					StringBuffer sb = new StringBuffer();
-					for (int i = 0; i < args.length - 1; i++)
-						sb.append(args[i] + " ");
-					sb.append(args[args.length - 1]);
-					characterManager.draw(player, sb.toString());
-				}
-			}
-		}
-		return false;
 	}
 
 	public CharacterManager getManager() {
